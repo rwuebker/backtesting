eb502cc3e7b49e7436795eff5755ad0d78425cbd
rwuebker <rlwuebker@gmail.com>
2019-08-17 20:05:19 -0700

    adds risk_model.py

---
+++ libs/risk_model.py
@@ -0,0 +1,25 @@
+
+
+
+class RiskModel:
+
+    def __init__(self, returns):
+        pass
+
+    def calc_factor_exposures(self):
+        pass
+
+    def calc_factor_returns(self):
+        pass
+
+    def create_cov_matrix(self):
+        pass
+
+    def create_idiosyncratic_var_matrix(self):
+        pass
+
+    def get_idiosyncratic_var_vector(self):
+        pass
+
+
+
