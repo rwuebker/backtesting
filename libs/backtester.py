1e2e1226d88456decc88dc1e8ca07876727954a3
rwuebker <rlwuebker@gmail.com>
2019-08-18 22:38:53 -0700

    adds backester.py

--- libs/backtester.py
+++ libs/backtester.py
@@ -1,12 +1,23 @@
-
+import pandas as pd
+import datetime as dt
 
 class BackTester:
 
-    def __init__(self):
+    def __init__(self, universe=pd.DataFrame(), start_date, end_date):
+        self.universe = universe
+
         pass
 
-    def get_optimal_holdings(self):
-        pass
+    def get_optimal_holdings(self, date_):
+        universe = self.get_universe(date_)
+
+
+
+    def get_universe(self, date_):
+        # should adjust the universe to only stocks > 1B or ones we already have a position
+        return self.universe
+
+
 
     def build_tradelist(self):
         pass
