801e8b5887addefedaad8415147a016d9d8fdfe4
rwuebker <rlwuebker@gmail.com>
2019-08-20 22:46:19 -0700

    updates optimal_holdings

--- libs/optimal_holdings.py
+++ libs/optimal_holdings.py
@@ -3,10 +3,10 @@ import numpy as np
 import cvxpy as cvx
 
 
-class OptimalHoldings:
+class OptimalHoldingsCVX:
 
     def __init__(self, risk_model, alpha_vector, previous, risk_cap=0.05, factor_max=10.0, aum=50e6, lambda_reg=0.50,
-                 factor_min=-10.0, weights_max=0.02, weights_min=-0.02, risk_aversion=1.0e-6):
+                 factor_min=-10.0, weights_max=0.01, weights_min=-0.01, risk_aversion=1.0e-6):
 
         self.risk_model = risk_model
         self.alpha_vector = alpha_vector
@@ -24,38 +24,23 @@ class OptimalHoldings:
     def _get_obj(self):
         ra = self.risk_aversion
         Q = self.risk_model.Q # Q is k x N
-        i_var_vector = self.risk_model.i_var_vector
-        ivv = cvx.Parameter((len(i_var_vector), 1))
-        ivv.value = i_var_vector.values
-        self.ivv = ivv
+        ivv = self.risk_model.i_var_vector
         av = self.alpha_vector.values.reshape(len(self.alpha_vector), 1)
-        print('this is av.shape: ', av.shape)
-        print('this is self.previous.shape: ', self.previous.shape)
-        h0 = cvx.Parameter(len(self.previous))
-        h0.value = self.previous
+        h0 = self.previous
         lambda_vector = np.ones((len(av), 1)) * 0.1/2000000
-        lv = cvx.Parameter(lambda_vector.shape)
-        lv.value = lambda_vector
+        lv = lambda_vector
         h1 = cvx.Variable(len(self.alpha_vector))
-        multiplier = cvx.Parameter(nonneg=True)
-        multiplier.value = ra * 0.50
-        func = 0.0
-        func += multiplier * cvx.sum((Q @ h1) ** 2)
-        func +=  multiplier * ((h1**2) @ ivv)
-        func -= h1 @ av
-        func += ((h1-h0) ** 2) @ lv
         m = ra * 0.50
-        print(ivv.shape)
-        print(h1.shape)
-        #func = multiplier * cvx.sum((Q @ h1) ** 2) + multiplier * (h1**2) @ ivv - (h1 @ av) + ((h1-h0) ** 2) @ lv
-        func = m * cvx.sum((Q @ h1)**2) + m * ((h1**2) @ ivv) - (h1 @ av) + ((h1-h0)**2) @ lv
-        func = m * cvx.sum((Q @ h1)**2) + m * ((h1**2) @ ivv)
+        #func = m * cvx.sum((Q @ h1)**2) + m * ((h1**2) @ ivv) - (h1 @ av) + ((h1-h0)**2) @ lv + self.lambda_reg * cvx.norm(h1, 2)
+        func = -(h1 @ av) + self.lambda_reg * cvx.norm(h1, 2)
         func = cvx.Minimize(func)
         prob = cvx.Problem(func, [])
         print(prob.is_dcp())
-        av = -av
+        #av = -av
         self.h1 = h1
-        func = cvx.Minimize(av.T @ h1 + self.lambda_reg * cvx.norm(h1, 2))
+        self.ivv = ivv
+        self.Q = Q
+        #func = cvx.Minimize(av.T @ h1 + self.lambda_reg * cvx.norm(h1, 2))
         self.obj_func = func
 
     def _get_constraints(self):
@@ -70,13 +55,12 @@ class OptimalHoldings:
         c.append(factor_exposures.T @ h1 <= self.factor_max)
         c.append(factor_exposures.T @ h1 >= self.factor_min)
         c.append(sum(cvx.abs(h1)) <= 1.0)
-        return [risk <= self.risk_cap**2, factor_exposures.T @ h1 <= self.factor_max, factor_exposures.T @ h1 >= self.factor_min, \
-                sum(h1) == 0, sum(cvx.abs(h1)) <= 1, h1 >= self.weights_min, h1 <= self.weights_max]
+        return c
 
     def get_risk(self):
         Q = self.risk_model.Q
         h1 = self.h1
-        ivv = self.ivv
+        ivv = self.risk_model.i_var_vector
         result = cvx.sum((Q @ h1)**2) + (h1**2) @ ivv
         ivm = self.risk_model.i_var_matrix.values
         f = self.risk_model.factor_exposures.T @ h1
@@ -84,6 +68,8 @@ class OptimalHoldings:
         return cvx.quad_form(f, X) + cvx.quad_form(h1, ivm)
 
     def find(self):
+        h1 = self.h1
+        obj_func = self._get_obj()
         h1 = self.h1
         prob = cvx.Problem(self.obj_func, self._get_constraints())
         print(prob.is_dcp())
